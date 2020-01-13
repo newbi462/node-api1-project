b5cb2ca20d475dce1493e5a1c3bc4ac0db0a1790
newbi462 <isthisemailfree@gmail.com>
2020-01-13 15:42:57 -0500

    ENDPOINTS TO BUILD

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ server.get("/", function(request, response) {
   response.send({meg: `I am running on ${port}`});
 });
 
+//POST	/api/users	Creates a user using the information sent inside the request body.
+
 //GET	/api/users	Returns an array of all the user objects contained in the database.
 server.get("/api/users", function(request, response) {
   ProjectData.find()
@@ -39,6 +41,13 @@ server.get("/api/users", function(request, response) {
       )
     })
 });
+
+//GET	/api/users/:id	Returns the user object with the specified id.
+
+//DELETE	/api/users/:id	Removes the user with the specified id and returns the deleted user.
+
+//PUT	/api/users/:id	Updates the user with the specified id using data from the request body. Returns the modified document, NOT the original.
+
 
 
 //LISTEN SERVER
