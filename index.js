0cff87c76484473e1296ad4e89a985044629290a
newbi462 <isthisemailfree@gmail.com>
2020-01-13 15:34:46 -0500

    Blank server on 8000 ready to make endpoints

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // implement your API here
+
+
+//IMPORTS
+//Dependancies
+const express = require("express");
+
+// Import Data
+const ProjectData = require("./data/db.js");
+
+
+//START WITH EXPRESS
+const server = express();
+
+
+//MIDDLE WARE
+server.use(express.json());
+
+
+//ENDPOINTS
+//defult is running mesage
+server.get("/", function(request, response) {
+  response.send({meg: `I am running on ${port}`});
+});
+
+
+//LISTEN SERVER
+const port = 8000;
+server.listen(port, () => console.log((`\n ** api on: ${port} ** \n`)));
